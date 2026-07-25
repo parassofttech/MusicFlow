d29494e7d6d44fb4a4bd28cf87b676716d17e317
Paras Sahu <paras2115s@gmail.com>
2026-07-25 23:24:52 +0530

    indipop

--- src/newSong/IndipopSongs.jsx
+++ src/newSong/IndipopSongs.jsx
@@ -146,7 +146,7 @@ const indipopSongs = [
     artist: "Anuv Jain",
     cover:
       "https://i.scdn.co/image/ab67616d0000b2736cb8d8c7a7b2c2d6e8d0c4f3",
-    url: "https://example.comuhfgj/baarishein.mp3",
+    url: "https://example.comuhfgjdrgrthbfg/baarishein.mp3",
 },
 {
     id: "i15",
@@ -154,30 +154,30 @@ const indipopSongs = [
     artist: "Prateek Kuhad",
     cover:
       "https://i.scdn.co/image/ab67616d0000b273g9840lkfd3457945479w3c1d2e3f4051627384950a1b2",
-    url: "https://example.coxcjnvcm/jo-tum-mere-ho.mp",
+    url: "https://example.coxcjnvcm/jdgfhgfo-tum-mere-ho.mp",
 },
 {
     id: "i16",
     title: "Kho Gaye Hum Kahan",
     artist: "Jasleen Royal",
     cover:
-      "https://i.scdn.co/imajvdfvndfkvdfvdfge/ab67616d0000b273d3e465465f50617283940a1b2c3d4",
-    url: "https://example.kdhfmghdkfnlkcom/kho-gaye-hum-kahan.mp3",
+      "https://i.scdn.co/imajvdfvndfkvdfvdfge/5464gfbab67616d0000b273d3e465465f50617283940a1b2c3d4",
+    url: "https://example.kdhfmghdkfnlgfhkcom/kho-gaye-hum-kahan.mp3",
 },
 {
     id: "i17",
     title: "Raatan Lambiyan (Indie Version)",
     artist: "Various Artists",
     cover:
-      "https://i.scdn.co/image/ab67616d0000b273e5f60718293a4b5c6d7e8f901",
-    url: "https://example.com/raatan-lambiyan.mp3",
+      "https://i.scdn.co/image/ab67616d0gtfgbh000b273e5f60718293a4b5c6d7e8f901",
+    url: "https://example.com/raatan-gglambiyan.mp3",
 },
 {
     id: "i18",
     title: "Aise Kyun",
     artist: "Mismatched OST",
     cover:
-      "https://i.scdn.co/image/ab67616d0000b273f60718293a4b5c6d7e8f90123",
+      "https://i.scdn.co/image/ab67616d0000b2fghr64573f60718293a4b5c6d7e8f90123",
     url: "https://example.coghjfgdhgfm/aise-kyun.mp3",
 },
 {
