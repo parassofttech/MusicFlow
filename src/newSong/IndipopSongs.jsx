6cf1a87dec4a0f82ef0ea2a291820f86ca25e6e1
Paras Sahu <paras2115s@gmail.com>
2026-08-09 23:28:23 +0530

    indipop

--- src/newSong/IndipopSongs.jsx
+++ src/newSong/IndipopSongs.jsx
@@ -89,7 +89,7 @@ const indipopSongs = [
     title: "Alag Aasmaan",
     artist: "Anuv Jain",
     cover:
-      "https://i.scdn.co/image/ab67616d0000b273d7d3b4d8a8c6c0cfsdfsdghf5sdfse0b6e3c1",
+      "https://i.scdn.co/image/ab67616d0000b273d7d3b4d8a8c6c0cregdrfsdfsdghf5sdfse0b6e3c1",
     url: "https://example.com/alag-avfdfdfdsasmfghfaan.mp3",
   },
   {
@@ -97,15 +97,15 @@ const indipopSongs = [
     title: "Baarishein",
     artist: "Anuv Jain",
     cover:
-      "https://i.scdn.co/image/ab67616d0000b2736cb8d8c7a7b2csdf2d6e8d0c4f3",
-    url: "https://example.com/baardfsdishein.mp3",
+      "https://i.scdn.co/image/ab67616d0000b2736cb8d8c7a7b2csdf2fdgfd6e8d0c4f3",
+    url: "https://example.com/baardfsdidgsdshein.mp3",
   },
   {
     id: "i8",
     title: "Gul",
     artist: "Anuv Jain",
     cover:
-      "https://i.scdn.co/image/ab67616d0000b2738b3f6f3f4c4f5d5sdfsda5b5c5d5e",
+      "https://i.scdn.co/image/ab67616d0000b2738b3f6f3f4c4f5d5sdsdgfsda5b5c5d5e",
     url: "https://pagalnew.com/128-ddfsdfownloads/23347",
   },
   {
